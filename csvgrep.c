0959890a17a844c7a4cf57f43478fdc018f6abb1
Kirill Lastovirya <kirill.lastovirya@gmail.com>
2016-01-26 02:02:43 +0300

    working on csv parsing state machine

--- csvgrep.c
+++ csvgrep.c
@@ -3,23 +3,46 @@
 #include <stdlib.h>
 #include <getopt.h>
 
-//----------------------------------------------------
-// Data structures
-//----------------------------------------------------
-struct Args_type {
+//---------------------------------------------------
+// Arguments parsing
+//---------------------------------------------------
+
+typedef struct  {
   char * delimiter;
   char * fileName;
-} ;
+}  Args;
 
-typedef struct Args_type Args;
-
-void print(Args * args) {
+void printArgs(Args * args) {
   printf("delimiter=%s fileName=%s\n", args->delimiter, args->fileName);
 };
 
+void printUsageAndExit() {
+  printf("Usage: csvgrep -d 'delimiter' [-f 'fileName']\n");
+  exit(EXIT_SUCCESS);
+}
+
+void parseArguments(Args * args, int argc, char **argv) {
+  int opt;
+  while ((opt = getopt(argc, argv, "d:f:")) != -1) {
+    switch (opt) {
+      case 'd':        
+        args->delimiter = optarg;
+        break;        
+      case 'f':
+        args->fileName = optarg;
+        break;
+      default:
+        printUsageAndExit();
+    }
+  }
+  printArgs(args);
+}
+
+
 //---------------------------------------------------
-// Checks whether are we reading from stdin or from file
+// Methods to work with streams
 //---------------------------------------------------
+
 FILE * resolveStream(Args * args) {
   if (args->fileName == NULL) {
     return stdin;
@@ -33,19 +56,12 @@ FILE * resolveStream(Args * args) {
   }
 }
 
-//---------------------------------------------------
-// Close stream if it is a file
-//---------------------------------------------------
 void closeStream(FILE * stream) {
   if (stream != stdin && stream != NULL) {
     fclose(stream);
   }
 }
 
-//---------------------------------------------------
-// reads line by line and calls a callback
-// frees the line pointer after work
-//---------------------------------------------------
 void readByLine(FILE * stream, void (*callback)(char *, int)) {
   char *line = NULL;
   size_t len = 0;
@@ -61,50 +77,89 @@ void readByLine(FILE * stream, void (*callback)(char *, int)) {
 }
 
 //---------------------------------------------------
-// Parses the csv
+// CSV parsing
 //---------------------------------------------------
-void parseLine(char * line, int size) {
-  printf("Read:%s", line);
-}
+typedef struct {
+  char delimiter;
+  int headerSize;
+  char ** headerNames;
+} CsvDescriptor;
 
-//---------------------------------------------------
-// Print usage and exit
-//---------------------------------------------------
+enum states { DELIMITER, IN_CELL, IN_QUOTES } state = IN_CELL;
 
-void printUsageAndExit() {
-  printf("Usage: csvgrep -d 'delimiter' [-f 'fileName']\n");
-  exit(EXIT_SUCCESS);
-}
+void parseLine(char * line, int size, CsvDescriptor * csvDescriptor) {
+  printf("Read:%s\n", line);
+  char *p;
+  int c = (unsigned char) *p;
+  char * start_of_word = p;
 
-//---------------------------------------------------
-// Process args
-//---------------------------------------------------
-void parseArguments(Args * args, int argc, char **argv) {
-  int opt;
-  while ((opt = getopt(argc, argv, "d:f:")) != -1) {
-    switch (opt) {
-      case 'd':        
-        args->delimiter = optarg;
-        break;        
-      case 'f':
-        args->fileName = optarg;
-        break;
-      default:
-        printUsageAndExit();
+  for (p = line; *p != '\0'; p++) {
+    c = (unsigned char) *p; /* convert to unsigned char for is* functions */
+    switch (state) {
+      case IN_CELL:
+        if (c == '"') {
+          start_of_word = p + 1;
+          state = IN_QUOTES;
+          continue;
+        }
+        if (c == csvDescriptor->delimiter) {
+          state = DELIMITER;
+          //TODO process word from start_of_word to p-1          
+          continue;
+        }
+        continue; 
+
+      case IN_QUOTES:
+        if (c == '"') {
+          state = IN_CELL;
+          //TODO process the word from start_of_word to p-1
+          continue;
+        }
+        continue;
+
+      case DULL: 
+        if (c == csvDescriptor->delimiter) {
+          continue;
+        }
+        if (c == '"') {
+          state = IN_CELL;
+          start_of_word = p + 1; /* word starts at *next* char, not this one */
+          continue;
+        }
+        state = IN_CELL;
+        start_of_word = p; /* word starts here */
+        continue;
+
     }
   }
-  print(args);
+
+  if (csvDescriptor->headerSize == 0) {
+    //first run, we are initializing headers
+
+  }
 }
+
+
 //---------------------------------------------------
 // MAIN
 //---------------------------------------------------
 int main(int argc, char **argv) {
-  Args args;
-  parseArguments(&args, argc, argv);
+  //----- Working part
+ //  Args args;
+ //  parseArguments(&args, argc, argv);
 
- FILE *stream = resolveStream(&args);
- readByLine(stream, parseLine);
- closeStream(stream);
+ // FILE *stream = resolveStream(&args);
+ // readByLine(stream, parseLine);
+ // closeStream(stream);
+ //---------Tests-------
+  char * sampleCsvLines[2] = {"a;b;c", "00;11;22"};
+  //TODO make default value
+  CsvDescriptor csvDescriptor = {';', 0, null};
+  
+  for (int i = 0; i < 2; i ++) {
+    char * line = sampleCsvLines[i];
+    parseLine(line, sizeof(line), &csvDescriptor);
+  }
  
  exit(EXIT_SUCCESS);
 }
